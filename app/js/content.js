d8c367e1b21042a9c4b71de77fb8b1575a8e41a3
ShaneXie <shane369@gmail.com>
2016-03-06 11:39:14 -0600

    toggle button inserted

---
+++ app/js/content.js
@@ -0,0 +1,36 @@
+(function() {
+	var chkActionItv = null;
+	var chkChatItv = null;
+
+	var isChatReady = false
+	var msgs = $(".chat-lines");
+	var dmBtn = "<span><a class='button primary dark' id='dmTogglelBtn'><span>Turn on Danmaku</span></a></span>"
+	
+	$( document ).ready(function () {
+		chkActionItv = setInterval(insertToggleBtn, 500);
+		chkChatItv = setInterval(checkChat, 500);
+	});
+
+	function checkChat () {
+		if ( $(".chat-lines").length ) {
+    		clearInterval(chkChatItv);
+    		isChatReady = true;
+    	};
+	}
+    
+    function insertToggleBtn () {
+    	if ( $(".channel-actions").length ) {
+    		clearInterval(chkActionItv);
+    		$(".channel-actions").append(dmBtn);
+    		$( "#dmTogglelBtn" ).click(toggleDanmu);
+    	};
+    }
+
+    function toggleDanmu () {
+    	if (isChatReady) {
+    		alert("Chat room ready..");
+    	} else{
+    		alert("Loading chat room, please wait a second..");
+    	}
+    }
+})();
