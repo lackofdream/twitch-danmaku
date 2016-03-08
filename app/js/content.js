46178893cbcb87ae0d7eb79b998c7a96829b00c5
ShaneXie <shane369@gmail.com>
2016-03-08 00:49:55 -0600

    danmaku alg first attempt

--- app/js/content.js
+++ app/js/content.js
@@ -25,53 +25,74 @@
 	var chatOb = new MutationObserver(newChatMsgHandler);
 	var chatObConfig = { childList: true };
 
+	var disp = null;
+
 	var dmBtn = "<span><a class='button primary dark' id='dmTogglelBtn'><span>Turn Danmaku OFF</span></a></span>"
 
 	/***************Construct Danmaku Object*********************/
 	var Danmaku = function (basicInfo) {
 		this.color = basicInfo.color;
 		this.content = basicInfo.content;
-		this.dmLength = basicInfo.dmLength;
-		this.markUp = "<span class='danmaku-item' style='color:"+ this.color +"'>"+ this.content +"</span>";
+		this.speed = 50;
+		this.id = "dm" + Date.now();
+		this.markUp = "<span>I am a moe danmaku.</span>";
 	}
 
-	Danmaku.prototype.emit = function() {
+	Danmaku.prototype.updateMarkup = function(top, left) {
+		this.markUp = "<span id='this.id' class='danmaku-item'"+
+			" style='color:"+ this.color +";top="+top+"px;left="+left+"px;'>"+ this.content +"</span>";
+	}
+
+	Danmaku.prototype.putOnScreen = function() {
 		$(".player-fullscreen-overlay").append(this.markUp);
 	};
+
 	/************************************************************/
 
 	/********************** Danmaku dispatcher **********************/
-	var dmDispatcher = function () {
-		this.containerH = $(".player-fullscreen-overlay").clientHeight;
-		this.containerW = $(".player-fullscreen-overlay").clientWidth;
+	var DMDispatcher = function () {
+		this.containerH = $(".player-fullscreen-overlay").height();
+		this.containerW = $(".player-fullscreen-overlay").width();
 
 		this.tracks = new Array(Math.floor(this.containerH/25));
 		for (var i = 0; i < this.tracks.length; i++) {
 			this.tracks[i] = {
 				ready: true,
-				queue: []
+				waitTime: 0,
+				top: i*25 + 15
 			}
 		}
 	}
 
-	dmDispatcher.prototype.run = function() {
-		// body...
-	};
-
-	dmDispatcher.prototype.enterQ = function(dm) {
+	DMDispatcher.prototype.enterQ = function(dm) {
 		var findOneSpot = false;
+		var idx = 0;
 		for (var i = 0; i < this.tracks.length; i++) {
 			if (this.tracks[i].ready) {
-				this.tracks[i].queue.push(dm);
+				idx = i;
 				findOneSpot = true;
 				break;
 			}
 		}
 		if (!findOneSpot) {
-			this.tracks[Math.floor(Math.random()*this.tracks.length)].queue(push);
+			idx = Math.floor(Math.random()*this.tracks.length);
 		}
+		dm.top = this.tracks[idx].top;
+		dm.updateMarkup(this.tracks[idx].top, this.containerW);
+		dm.putOnScreen();
+		var currentWaitTime = this.tracks[idx].waitTime;
+		var dmLength = $("#"+dm.id).width();
+		this.tracks[idx].waitTime += (dmLength/(this.containerW/10000));
+		var scrollLength = dmLength + this.containerW;
+		$("#"+dm.id)
+			.delay(currentWaitTime)
+			.animate({
+				"left": "-="+scrollLength+"px"
+			},{
+				duration: 10000,
+				complete: function () { $(this).remove(); }
+			});
 	};
-
 
 	/****************************************************************/
 	
@@ -130,15 +151,14 @@
 						currNode = mutation.addedNodes[i];
 						var newDanmaku = {
 							color: currNode.querySelector("li .from").style.color,
-							content: currNode.querySelector("li .message").innerHTML,
-							dmLength: 0
+							content: currNode.querySelector("li .message").innerHTML
 						}
 						// calculate danmaku length
-						var emojiCount = currNode.querySelector("li .message").querySelectorAll("img").length;
-						var pureTextLength = currNode.querySelector("li .message").textContent.length;
-						newDanmaku.dmLength =  pureTextLength + emojiCount*2;
-
-						new Danmaku(newDanmaku).emit();
+						// var emojiCount = currNode.querySelector("li .message").querySelectorAll("img").length;
+						// var pureTextLength = currNode.querySelector("li .message").textContent.length;
+						// newDanmaku.dmLength =  pureTextLength + emojiCount*2;
+						disp.enterQ(new Danmaku(newDanmaku));
+						//new Danmaku(newDanmaku).emit();
 					}
 				};
 			};
@@ -148,7 +168,7 @@
     function startDanmaku () {
     	console.log("Start Danmaku.");
     	isDanmakuOn = true;
-    	
+    	disp = new DMDispatcher();
        	var chatRoom = document.querySelector('.chat-lines');
     	if ( chatRoom !== null ) {
     		console.log("Start observing.");
