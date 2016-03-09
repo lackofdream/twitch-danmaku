916e3d309b7c7c558fa586411c0a84b03e11772e
ShaneXie <shane369@gmail.com>
2016-03-09 16:20:47 -0600

    only run in streaming page

--- app/js/background.js
+++ app/js/background.js
@@ -1,7 +1,12 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(function() {
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-	    chrome.tabs.sendMessage(tabs[0].id, {action: "stateChange"}, function(response) {});  
-	});
+var reservedURL = 'directory jobs settings login signup logout';
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+	var urlParams = details.url.split('/');
+
+	if (urlParams.length===4 && reservedURL.indexOf(urlParams[3])===-1) {
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+		    chrome.tabs.sendMessage(tabs[0].id, {action: "stateChange"}, function(response) {});  
+		});
+	}
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
